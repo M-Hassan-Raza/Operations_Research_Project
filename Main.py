5496f3399c22c008e1a6b3361b79c831573a00ac
hassan <hassan@nu.edu.pk>
2022-12-11 01:12:17 +0500

    Added Custom Tkinter GUI

--- Main.py
+++ Main.py
@@ -1,13 +1,16 @@
 from tkinter import *
 import tkinter
 from tkinter import messagebox
+import customtkinter
 
-canvas = Tk()
+canvas = customtkinter.CTk()
 canvas.geometry('800x600')
 canvas.title('Linear Programming Solver')
 canvas.iconbitmap('res/line-chart.ico')
-canvas.configure(bg='pink')
 canvas.resizable(False, False)
+customtkinter.set_default_color_theme("green")
+customtkinter.set_appearance_mode("light")
+
 
 numberOfConstraints = StringVar()
 intConstraints = 0
@@ -20,15 +23,33 @@ nameOfProblem = StringVar()
 objectiveFunction = StringVar()
 problem = StringVar()
 
-Label(canvas, text='Number of constraints', font='SAN_SERIF 12').place(x=240, y=30)
-Entry(canvas, width=55, textvariable=numberOfConstraints, justify=CENTER).place(x=240, y=60)
 
-Label(canvas, text='Maximization or Minimization:', font='SAN_SERIF 12').place(x=240, y=120)
-Entry(canvas, width=55, textvariable=typeOfProblem, justify=CENTER).place(x=240, y=150)
+# menubar = Menu(canvas)
+# canvas.config(menu=menubar)
+# about_menu =
+# about_menu.add_command(label='About', command=show_about_menu)
+
+
+constLabel = customtkinter.CTkLabel(master=canvas, text='Number Of Constraints')
+constLabel.place(x=180, y=50)
+entry = customtkinter.CTkEntry(master=canvas, textvariable=numberOfConstraints)
+entry.pack(padx=20, pady=50)
+
+typeOfProblem = customtkinter.StringVar(value="Maximization")
+
+
+def combobox_callback(choice):
+    typeOfProblem = choice
+
+
+combobox = customtkinter.CTkComboBox(master=canvas, values=["Maximization", "Minimization"], command=combobox_callback,
+                                     variable=typeOfProblem).pack(padx=20, pady=10)
+typeProblemLabel = customtkinter.CTkLabel(master=canvas, text='Type of Problem')
+typeProblemLabel.place(x=180, y=140)
 
 
 def main_function():
-    if (not numberOfConstraints.get()) or (not typeOfProblem.get()):
+    if not numberOfConstraints.get():
         messagebox.showinfo('Error', 'Text boxes can\'t be empty')
         return
     elif typeOfProblem.get().lower() != 'maximization' and typeOfProblem.get().lower() != 'minimization':
@@ -42,31 +63,29 @@ def main_function():
             messagebox.showinfo('Error', 'Number of constraints must be an integer value')
             return
 
-    top = Toplevel()
+    top = customtkinter.CTkToplevel()
     top.geometry('800x600')
     top.title('Enter The Data')
     top.iconbitmap('res/form.ico')
-    top.configure(bg='pink')
     top.resizable(False, False)
 
-    l = Label(top, text='Objective Function: ')
+    l = customtkinter.CTkLabel(master=top, text='Objective Function')
     l.grid(row=0, column=0, padx=50, pady=10)
-    e = Entry(top)
+    e = customtkinter.CTkEntry(master=top, width=220)
     e.grid(row=0, column=1, padx=10, pady=10)
 
     for j in range(intConstraints):
-        l = Label(top, text='Constraint: ' + str(j + 1))
-        l.grid(row=j + 1, column=0, padx=10)
+        enumeratedLabel = customtkinter.CTkLabel(master=top, text='Constraint: ' + str(j + 1))
+        enumeratedLabel.grid(row=j + 1, column=0, padx=10)
 
-        e = Entry(top)
-        e.grid(row=j + 1, column=1, padx=10, pady=10)
+        enumeratedEntry = customtkinter.CTkEntry(master=top, width=220)
+        enumeratedEntry.grid(row=j + 1, column=1, padx=10, pady=10)
 
-    Button(top, text='Confirm',
-                   command=main_function, font=('SAN_SERIF', 15, 'bold'), relief=FLAT,
-                   activebackground='black', activeforeground='white').place(x=300, y=300)
+    customtkinter.CTkButton(master=top, text='Confirm', corner_radius=8,
+                            command=main_function, font=('SAN_SERIF', 15, 'bold')).place(x=300, y=300)
 
-tkinter.Button(canvas, text='Confirm',
-               command=main_function, font=('SAN_SERIF', 15, 'bold'), relief=FLAT,
-               activebackground='black', activeforeground='white').place(x=300, y=300)
+
+customtkinter.CTkButton(master=canvas, text='Confirm', corner_radius=8,
+                        command=main_function, font=('SAN_SERIF', 15, 'bold')).place(x=300, y=300)
 
 canvas.mainloop()
