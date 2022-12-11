12571374b4e64c365f3a23597b222e9a947a336b
hassan <hassan@nu.edu.pk>
2022-12-11 13:31:50 +0500

    Added Flags and added final palceholder functions

--- Main.py
+++ Main.py
@@ -1,7 +1,13 @@
 from tkinter import *
+import tkinter
 from tkinter import messagebox
-import customtkinter
+from pulp import *
 
+import PIL
+import customtkinter
+from PIL import Image, ImageTk
+
+#
 canvas = customtkinter.CTk()
 canvas.geometry('800x600')
 canvas.title('Linear Programming Solver')
@@ -10,29 +16,39 @@ canvas.resizable(False, False)
 customtkinter.set_default_color_theme("green")
 customtkinter.set_appearance_mode("light")
 
+canvas_open = True
+entry_form_open = False
+final_window_open = False
+
 
 numberOfConstraints = StringVar()
+nameOfProblem = StringVar()
 intConstraints = 0
+top = ''
 typeOfProblem = StringVar()
 listOfConstraints = []
 listOfDecisionVariables = []
-listOfLowerBounds = []
-listOfUpperBounds = []
+lowerBound = 0
 nameOfProblem = StringVar()
 objectiveFunction = StringVar()
 problem = StringVar()
-
 
 # menubar = Menu(canvas)
 # canvas.config(menu=menubar)
 # about_menu =
 # about_menu.add_command(label='About', command=show_about_menu)
 
+constLabel = customtkinter.CTkLabel(master=canvas, text='Name of The Problem')
+constLabel.place(x=180, y=30)
+entry = customtkinter.CTkEntry(master=canvas, textvariable=nameOfProblem)
+entry.pack(padx=20, pady=30)
+
 
 constLabel = customtkinter.CTkLabel(master=canvas, text='Number Of Constraints')
-constLabel.place(x=180, y=50)
+constLabel.place(x=180, y=120)
 entry = customtkinter.CTkEntry(master=canvas, textvariable=numberOfConstraints)
-entry.pack(padx=20, pady=50)
+entry.pack(padx=20, pady=30)
+
 
 typeOfProblem = customtkinter.StringVar(value="Maximization")
 
@@ -44,11 +60,8 @@ def combobox_callback(choice):
 combobox = customtkinter.CTkComboBox(master=canvas, values=["Maximization", "Minimization"], command=combobox_callback,
                                      variable=typeOfProblem).pack(padx=20, pady=10)
 typeProblemLabel = customtkinter.CTkLabel(master=canvas, text='Type of Problem')
-typeProblemLabel.place(x=180, y=140)
+typeProblemLabel.place(x=180, y=190)
 
-
-def confirm_constraints():
-    pass
 
 def main_function():
     if not numberOfConstraints.get():
@@ -67,13 +80,13 @@ def main_function():
 
     canvas.withdraw()
     top = customtkinter.CTkToplevel()
-    top.geometry('800x600')
+    top.geometry('900x900')
     top.title('Enter The Data')
     top.iconbitmap('res/form.ico')
-    top.resizable(False, False)
+    top.resizable(True, True)
 
     labelLooped = customtkinter.CTkLabel(master=top, text='Objective Function')
-    labelLooped.grid(row=0, column=0, padx=50, pady=10)
+    labelLooped.grid(row=0, column=0, padx=80, pady=10)
     entryLooped = customtkinter.CTkEntry(master=top, width=220)
     entryLooped.grid(row=0, column=1, padx=10, pady=10)
 
@@ -84,11 +97,39 @@ def main_function():
         enumeratedEntry = customtkinter.CTkEntry(master=top, width=220)
         enumeratedEntry.grid(row=j + 1, column=1, padx=10, pady=10)
 
-    customtkinter.CTkButton(master=top, text='Confirm', corner_radius=8,
-                            command=confirm_constraints, font=('SAN_SERIF', 15, 'bold')).place(x=300, y=300)
+        listOfDecisionVariables[j] = 'X' + str(j+1)
+
+
+    lowerBoundLabel = customtkinter.CTkLabel(master=top, text='All Xs >= ')
+    lowerBoundLabel.grid(row=intConstraints + 2, column=0, padx=10)
+
+    lowerBoundEntry = customtkinter.CTkEntry(master=top, width=220)
+    lowerBoundEntry.grid(row=intConstraints + 2, column=1, padx=10, pady=10)
+    def solve_problem():
+        pass
+
+    def go_back():
+        top.withdraw()
+        canvas.iconify()
+        pass
+
+    def confirm_constraints():
+        top.withdraw()
+        finalWindow = customtkinter.CTkToplevel()
+        finalWindow.geometry('900x900')
+        finalWindow.title('Enter The Data')
+        finalWindow.iconbitmap('res/form.ico')
+        finalWindow.resizable(True, True)
+
+
+    customtkinter.CTkButton(master=top, text='Confirm Constraints', corner_radius=8,
+                            command=confirm_constraints, font=('SAN_SERIF', 15, 'bold')).place(x=320, y=520)
+
+    prev_button = customtkinter.CTkButton(master=top, text='←', command=go_back, width=32, height=32, corner_radius=100)
+    prev_button.place(x=10, y=10)
 
 
 customtkinter.CTkButton(master=canvas, text='Confirm', corner_radius=8,
-                        command=main_function, font=('SAN_SERIF', 15, 'bold')).place(x=300, y=300)
+                        command=main_function, font=('SAN_SERIF', 15, 'bold')).place(x=320, y=420)
 
 canvas.mainloop()
